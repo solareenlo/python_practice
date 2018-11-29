1a253ea3be2f50255d815bd79d279ff56f333f27
solareenlo <solareenlo@gmail.com>
2018-11-30 03:49:54 +0900

    decoratorは対象関数に前後処理を付け加える時に用いる

---
+++ 03_制御フローとコード構造/decorator1.py
@@ -0,0 +1,23 @@
+"""This is a test program."""
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return wrapper
+
+@print_info # add_numをprint_infoでデコテートするということ
+def add_num(a, b):
+    return a + b
+r = add_num(10, 20)
+print(r) # start end 30 と縦に表示
+
+# 上記の5行と下記の5行は同じこと
+# def add_num(a, b):
+#     return a + b
+# f = print_info(add_num)
+# r = f(10, 20)
+# print(r) # start end 30 と縦に表示
+
+# デコレーターとは, 対象関数(ここではadd_num)に前後処理を付け加えたい時に使う
