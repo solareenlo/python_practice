2f3df38d982251546668226ab47269b557ca9725
solareenlo <solareenlo@gmail.com>
2018-12-05 17:49:15 +0900

    とりあえずオススメのレストランを尋ねる

--- 07_入門編の終了_応用編に行く前に簡単なアプリの演習/maim.py
+++ 07_入門編の終了_応用編に行く前に簡単なアプリの演習/maim.py
@@ -9,7 +9,10 @@ import termcolor
 
 ROBOT_NAME = 'Robobo'
 ROBOT_COLOR = 'green'
-FIELDNAMES = ['NAME', 'COUNT']
+CSV = 'ranking.csv'
+NAME = 'Restaurant Name'
+COUNT = 'Count'
+FIELDNAMES = [NAME, COUNT]
 
 # 会話内容を納めたファイルへのファイルパスを設定
 FILE_PASS = os.path.dirname(os.path.abspath(__file__))
@@ -29,6 +32,43 @@ with open(HELLO_FILE_PASS, 'r', encoding='utf-8') as template_file:
 # ユーザーの名前を取得
 USER_NAME = input(CONTENTS.substitute({'robot_name': ROBOT_NAME}))
 
+# csvファイルのデータを格納する箱を用意
+DATA = collections.defaultdict(int)
+
+# ranking.csvが無い場合は作成する
+if not os.path.exists(CSV):
+    pathlib.Path(CSV).touch()
+
+# csvファイルを開く
+with open(CSV, 'r+') as csv_file:
+    READER = csv.DictReader(csv_file)
+    for row in READER:
+        DATA[row[NAME]] = int(row[COUNT])
+
+# オススメのレストランを尋ねる
+# SORTED_DATA = sorted(DATA, key=DATA.get, reverse=True)
+# print(SORTED_DATA[0])
+if DATA:
+    while True:
+        RECOMMEND_RESTAURANT_FILE_PASS = os.path.join(FILE_PASS, 'greeting.md')
+        SORTED_DATA = sorted(DATA, key=DATA.get, reverse=True)
+        NEW_RECOMMEND_RESTAURANT = SORTED_DATA[0]
+        with open(RECOMMEND_RESTAURANT_FILE_PASS, 'r', encoding='utf-8') as template_file:
+            CONTENTS = template_file.read()
+            CONTENTS = CONTENTS.rstrip(os.linesep)
+            CONTENTS = '{splitter}{sep}{contents}{sep}{splitter}{sep}'.format(
+                contents=CONTENTS, splitter="=" * 64, sep=os.linesep)
+            CONTENTS = termcolor.colored(CONTENTS, ROBOT_COLOR)
+            CONTENTS = string.Template(CONTENTS)
+        IS_YES = input(CONTENTS.substitute({
+            'robot_name': ROBOT_NAME,
+            'user_name': USER_NAME,
+            'restaurant': NEW_RECOMMEND_RESTAURANT}))
+        if IS_YES.lower() == 'y' or IS_YES.lower() == 'yes':
+            break
+        if IS_YES.lower() == 'n' or IS_YES.lower() == 'no':
+            break
+
 # どのレストランが好きか尋ねる
 WHICH_FILE_PASS = os.path.join(FILE_PASS, 'which_restaurant.md')
 with open(WHICH_FILE_PASS, 'r', encoding='utf-8') as template_file:
@@ -42,31 +82,15 @@ with open(WHICH_FILE_PASS, 'r', encoding='utf-8') as template_file:
 # 好きなレストラン名を取得
 RESTARANT = input(CONTENTS.substitute({'robot_name': ROBOT_NAME, 'user_name': USER_NAME}))
 
-HANTEI = False
-COUNTER = 0
-
-# csvファイルのデータを格納する箱を用意
-DATA = collections.defaultdict(int)
-
-# ranking.csvが無い場合は作成する
-if not os.path.exists('ranking.csv'):
-    pathlib.Path('ranking.csv').touch()
-
-# csvファイルを開く
-with open('ranking.csv', 'r+') as csv_file:
-    READER = csv.DictReader(csv_file)
-    for row in READER:
-        DATA[row['NAME']] = int(row['COUNT'])
-
 # レストランの数を1つ増やす
 DATA[RESTARANT.title()] += 1
 
 # csvファイルに保存
-with open('ranking.csv', 'w+') as csv_file:
+with open(CSV, 'w+') as csv_file:
     WRITER = csv.DictWriter(csv_file, fieldnames=FIELDNAMES)
     WRITER.writeheader()
     for name, count in DATA.items():
-        WRITER.writerow({'NAME': name, 'COUNT': count})
+        WRITER.writerow({NAME: name, COUNT: count})
 
 # 最後の挨拶を表示
 THANKS_FILE_PASS = os.path.join(FILE_PASS, 'thanks.md')
---
+++ 07_入門編の終了_応用編に行く前に簡単なアプリの演習/robot/templates/greeting.md
@@ -0,0 +1,5 @@
+$robot_name: 私のオススメのレストランは, $restaurantです.
+$robot_name: I recommend $restaurant restaurant.
+
+$robot_name: このレストランはお好きですか？ [Yes/No]
+$robot_name: Do you like it? [y/n]
