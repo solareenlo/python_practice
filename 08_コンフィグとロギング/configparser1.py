50780680b63c71ff287aaa35e0445dcc9deb774f
solareenlo <solareenlo@gmail.com>
2018-12-06 10:26:58 +0900

    configparserでコンフィグファイルを扱う

--- 08_コンフィグとロギング/configparser1.py
+++ 08_コンフィグとロギング/configparser1.py
@@ -1,6 +1,17 @@
 """ configparserはコンフィグファイルを扱うものです. """
 import configparser
+""" configparserで扱う形式は以下のような形
+[DEFAULT]
+debug = True
 
+[web_server]
+host = 127.0.0.1
+port = 80
+
+[db_server]
+host = 127.0.0.1
+port = 3306
+"""
 # コンフィグの書き込み方法
 # CONFIG = configparser.ConfigParser()
 # CONFIG['DEFAULT'] = {
@@ -18,10 +29,10 @@ import configparser
 #     CONFIG.write(config_file)
 
 # コンフィグの読み込み方法
-config = configparser.ConfigParser()
-config.read('config.ini')
-print(config['web_server']) # <Section: web_server> と表示
-print(config['web_server']['host']) # 127.0.0.1 と表示
-print(config['web_server']['port']) # 80 と表示
+CONFIG = configparser.ConfigParser()
+CONFIG.read('config.ini')
+print(CONFIG['web_server']) # <Section: web_server> と表示
+print(CONFIG['web_server']['host']) # 127.0.0.1 と表示
+print(CONFIG['web_server']['port']) # 80 と表示
 
-print(config['DEFAULT']['debug']) # True と表示
+print(CONFIG['DEFAULT']['debug']) # True と表示
